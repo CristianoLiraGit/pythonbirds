2433004f2e52ff6c08ff4bd07f8521c1195b0e10
CristianoLiraGit <cristiano_sclira@hotmail.com>
2022-10-23 00:44:56 -0300

    Criado o método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,12 @@
 # classe é o que define como os objetos se comportam
 class Pessoa:
-    pass
+    def cumprimentar(self): # definir um método. O método pertence a uma classe, atralado a um objeto
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+# método é uma função que pertence à classe
